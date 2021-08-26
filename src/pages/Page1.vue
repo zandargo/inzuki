b887c2193ef92dd9f390cd93fe041048484c2b6f
Madson Germano <madson.br@gmail.com>
2021-08-26 16:10:46 -0300

    WIP

--- src/pages/Page1.vue
+++ src/pages/Page1.vue
@@ -2,24 +2,12 @@
 	<div>
 		<q-page class="">
 			<div class="column justify-start items-center">
-				<div class="col">
-					<h1>Heading h1</h1>
-				</div>
-				<div class="col">
-					<h2>Heading h2</h2>
-				</div>
-				<div class="col">
-					<h3>Heading h3</h3>
-				</div>
-				<div class="col">
-					<h4>Heading h4</h4>
-				</div>
-				<div class="col">
-					<h5>Heading h5</h5>
-				</div>
-				<div class="col">
-					<h6>Heading h6</h6>
-				</div>
+				<div class="col text-h1">Heading h1</div>
+				<div class="col text-h2">Heading h2</div>
+				<div class="col text-h3">Heading h3</div>
+				<div class="col text-h4">Heading h4</div>
+				<div class="col text-h5">Heading h5</div>
+				<div class="col text-h6">Heading h6</div>
 			</div>
 		</q-page>
 	</div>
--- src/pages/Page2.vue
+++ src/pages/Page2.vue
@@ -1,28 +1,36 @@
 <template>
 	<div>
-		<q-page class="column justify-start items-center q-pa-none">
-			<div class="col">
-				<q-card class="title-card q-ma-md" bordered elevated>
-					<q-card-section class="text-center text-justify q-pa-md">
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-						do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-						Ut enim ad minim veniam, quis nostrud exercitation ullamco
-						laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-						irure dolor in reprehenderit in voluptate velit esse cillum
-						dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-						cupidatat non proident, sunt in culpa qui officia deserunt
-						mollit anim id est laborum.
-					</q-card-section>
-				</q-card>
-			</div>
-			<div class="col">
-				<q-btn rounded color="primary" label="Clique aqui" />
-			</div>
-			<div class="col">
-				<q-btn-group rounded dense>
-					<q-btn rounded dense color="primary" icon="add" />
-					<q-btn rounded dense color="primary" icon="remove" />
-				</q-btn-group>
+		<q-page>
+			<div class="column justify-start items-center q-pa-md q-gutter-sm">
+				<div class="col">
+					<q-card class="title-card" bordered elevated>
+						<q-card-section class="q-py-xs bg-grey-2">
+							<div class="text-h3 q-ma-none">Lorem ipsum</div>
+						</q-card-section>
+						<q-separator inset class="text-h3" />
+						<q-card-section
+							class="text-center text-justify text-body1 q-pa-md"
+						>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+							sed do eiusmod tempor incididunt ut labore et dolore magna
+							aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+							ullamco laboris nisi ut aliquip ex ea commodo consequat.
+							Duis aute irure dolor in reprehenderit in voluptate velit
+							esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+							occaecat cupidatat non proident, sunt in culpa qui officia
+							deserunt mollit anim id est laborum.
+						</q-card-section>
+					</q-card>
+				</div>
+				<div class="col">
+					<q-btn rounded color="primary" label="Clique aqui" />
+				</div>
+				<div class="col">
+					<q-btn-group rounded dense>
+						<q-btn rounded dense color="primary" icon="add" />
+						<q-btn rounded dense color="primary" icon="remove" />
+					</q-btn-group>
+				</div>
 			</div>
 		</q-page>
 	</div>
